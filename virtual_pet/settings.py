3eaee7be6b95b254041786ee3b594983e5992847
Ivan <icarpiodeveloper@gmail.com>
2025-03-31 17:02:41 +0200

    Test001

--- virtual_pet/settings.py
+++ virtual_pet/settings.py
@@ -12,12 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 REDIS_URL = os.environ.get('REDIS_URL')
 #CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
-
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
